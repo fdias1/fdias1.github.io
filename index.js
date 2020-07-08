73bbada75894caecee0cda4b958eee5c2534f601
Fábio Dias <fabbiodiaz@gmail.com>
2020-07-07 22:36:55 -0300

    plugin de fade jQuery finalizado

--- index.js
+++ index.js
@@ -1,8 +1,58 @@
-console.log('hello world!')
-console.log($('.header'))
+function getElementOpcacity(element) {
+  const scrollTop = $(window).scrollTop()
+  const scrollHeight = $(window).height()
+  const scrollBotton = $(window).scrollTop() + scrollHeight
+  const elementTop = element.position().top
+  const elementHeight = element.height()
+  const elementBotton = element.position().top + elementHeight
+
+  const blackZone = 0.2
+  const fadeZone = 0.3
+
+  const blackZoneHeight = blackZone * scrollHeight
+  const blackZoneTop = scrollTop + blackZoneHeight
+  const blackZoneBotton = scrollBotton - blackZoneHeight
+
+  const fadeZoneHeight = fadeZone * scrollHeight
+  const fadeZoneTop = scrollTop + fadeZoneHeight + blackZoneHeight
+  const fadeZoneBotton = scrollBotton - fadeZoneHeight - blackZoneHeight
+
+  let whereElementIs = ''
+  if (elementBotton < scrollBotton && elementTop > scrollTop) {
+    whereElementIs = 'focus'
+    return 1
+
+  } else if (elementTop > blackZoneBotton){
+    whereElementIs = 'blackzone'
+    return 0
+    
+  } else if (elementBotton < blackZoneTop) {
+    whereElementIs = 'blackzone'
+    return 0
+
+  } else if (elementTop > fadeZoneBotton) {
+    whereElementIs = 'botton'
+    return (blackZoneBotton - elementTop) / fadeZoneHeight
+
+  } else if (elementBotton < fadeZoneTop ) {
+    whereElementIs = 'top'
+    return (elementBotton - blackZoneTop) / fadeZoneHeight
+
+  } else {
+    whereElementIs = 'focus'
+    return 1
+
+  }
+}
+
+function fade(element) {
+  element.children().css("opacity",getElementOpcacity($(element)))
+}
 
 $(document).ready(function(){
   $(window).scroll(function(){
-      $(".header>.container").css("opacity", 1 - $(window).scrollTop() / ($('.header').height()/2));
-  });
-});
+    $('section').each((index,element) => {
+      fade($(element))
+    })
+  })
+})
